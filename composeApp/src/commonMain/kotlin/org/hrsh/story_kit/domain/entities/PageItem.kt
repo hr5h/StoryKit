990f40e55d7aba7c2cb32e458936067729eb4c25
hr5h <71148972+hr5h@users.noreply.github.com>
2025-01-20 12:50:50 +0300

    add documentation for StoryItem, PageItem, StoryManager

--- composeApp/src/commonMain/kotlin/org/hrsh/story_kit/domain/entities/PageItem.kt
+++ composeApp/src/commonMain/kotlin/org/hrsh/story_kit/domain/entities/PageItem.kt
@@ -1,25 +1,60 @@
 package org.hrsh.story_kit.domain.entities
 
+/**
+ * Represents a page item that can be part of a story.
+ * This sealed class can have different types of page items.
+ */
 sealed class PageItem {
+
+    /**
+     * Represents an image page item.
+     *
+     * @property imageUrl A string representing the URL of the image that will be displayed
+     *                    as the main content.
+     * @property text A string that contains text to be shown at the bottom of the image.
+     * @property timeShow An integer representing the duration (in seconds) for which the
+     *                    story will be displayed.
+     */
     data class Image(
         val imageUrl: String,
         val text: String,
         val timeShow: Int = 5
     ) : PageItem()
 
+    /**
+     * Represents a video page item.
+     *
+     * @property videoUrl A string representing the URL of the video that will be displayed.
+     */
     data class Video(
         val videoUrl: String,
     ) : PageItem()
 
+    /**
+     * Represents a question page item.
+     *
+     * @property imageUrl A string representing the URL of the image associated with the question.
+     * @property question A string containing the question that will be displayed.
+     * @property listAnswers A list of strings representing the possible answers that can be selected.
+     */
     data class Question(
         val imageUrl: String,
         val question: String,
         val listAnswers: List<String>
     ) : PageItem()
 
+    /**
+     * Represents a game page item.
+     *
+     * @property name A string representing the name of the game.
+     */
     data class Game(
         val name: String,
     ) : PageItem()
 
+    /**
+     * Represents an error page item.
+     * This object signifies that an error has occurred.
+     */
     data object Error : PageItem()
 }
--- composeApp/src/commonMain/kotlin/org/hrsh/story_kit/domain/entities/StoryItem.kt
+++ composeApp/src/commonMain/kotlin/org/hrsh/story_kit/domain/entities/StoryItem.kt
@@ -1,17 +1,25 @@
 package org.hrsh.story_kit.domain.entities
 
+/**
+ * Represents a story item in the application.
+ *
+ * @property id A unique identifier for the story. It is used to access, add, delete,
+ *               or modify the story in the database. This key is also utilized for
+ *               receiving events related to this ID.
+ * @property imagePreview A string that holds the URL or path to the image that is
+ *                        displayed in the stories feed.
+ * @property listPages A list of [PageItem] that will be shown when the story is opened.
+ * @property isStartStory A boolean flag indicating whether the story should be opened
+ *                        upon app launch.
+ * @property isLike A boolean variable indicating whether the story has been liked.
+ * @property countLike An integer representing the number of likes the story has received.
+ * @property isFavorite A boolean variable indicating whether the story has been added
+ *                      to favorites.
+ * @property isViewed A boolean variable indicating whether the story has been viewed.
+ */
 data class StoryItem(
-    /**
-     * the id of your story for further tracking of events like, view, skip
-     */
     val id: Long,
-    /**
-     * the image that will be shown in the story miniature
-     */
     val imagePreview: String,
-    /**
-     * a list of pages that have a history
-     */
     val listPages: List<PageItem>,
     val isStartStory: Boolean = false,
     val isLike: Boolean = false,
