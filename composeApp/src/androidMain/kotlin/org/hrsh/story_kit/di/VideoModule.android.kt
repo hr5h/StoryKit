703ed7c8c929cb3538c84e2693c90b8e149fbfc2
hr5h <71148972+hr5h@users.noreply.github.com>
2025-03-19 21:52:07 +0300

    fix videoplayer, now it doesn't work on_stop app

--- composeApp/src/androidMain/kotlin/org/hrsh/story_kit/di/VideoModule.android.kt
+++ composeApp/src/androidMain/kotlin/org/hrsh/story_kit/di/VideoModule.android.kt
@@ -11,33 +11,54 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
 
 @Composable
 actual fun VideoPlayer(modifier: Modifier, url: String, isVisible: Boolean) {
     val context = LocalContext.current
+    val lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
+
     val player = remember {
         ExoPlayer.Builder(context).build().apply {
             val mediaItem = MediaItem.fromUri(Uri.parse(url))
             setMediaItem(mediaItem)
-            prepare()
             playWhenReady = true
         }
     }
 
     LaunchedEffect(isVisible) {
         if(isVisible) {
+            player.prepare()
             player.play()
         }
         else {
             player.stop()
             player.seekTo(0)
-            player.prepare()
         }
     }
 
     DisposableEffect(Unit) {
         onDispose {
             player.release()
+        }
+    }
+
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_START) {
+                player.play()
+            } else if (event == Lifecycle.Event.ON_STOP) {
+                player.pause()
+            }
+        }
+
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
         }
     }
 
